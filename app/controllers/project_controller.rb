69fc6b923b55a9f31e4c41c0e5d64524cb77e103
Trevor Preston <trevorpreston4@gmail.com>
2016-07-30 16:16:35 -0400

    added API get request.  renders to JSON

--- app/controllers/project_controller.rb
+++ app/controllers/project_controller.rb
@@ -1,4 +1,8 @@
 class ProjectController < ApplicationController
   def index
+    url = "https://api.github.com/repos/trevp3/Project_Democracy_DEMO_DATA/issues?state=all"
+    response = HTTParty.get(url)
+    parsed_body = JSON.parse(response.body)
+    render json: parsed_body
   end
 end
